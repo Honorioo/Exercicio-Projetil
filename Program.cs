ebf8e2ed7c4e4639f1cd53d3fc92e12edb42d071
Vinicius <viniciusgomeshonorio@hotmail.com>
2021-03-28 13:11:41 -0300

    Exercicio Projetil

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercicio_Projetil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double g = 9.80665;
+            double rad, velo, angulo, resultadoAltura, resultadoAlcance;
+
+            Console.Write("De a velocidade, em m/s...: ");
+            velo = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("De o angulo, em graus...: ");
+            angulo = Convert.ToDouble(Console.ReadLine());
+
+                rad = angulo * (Math.PI/180);
+
+                resultadoAlcance = Math.Pow(velo, 2) * Math.Sin (2*rad) / g;
+                resultadoAltura = Math.Pow(velo*Math.Sin(rad), 2) / (2*g);
+
+            Console.WriteLine();
+            Console.WriteLine($"Alcance...: {resultadoAlcance:N2}");
+            Console.WriteLine($"Altura...: {resultadoAltura:N2}");
+
+
+        }
+    }
+}
